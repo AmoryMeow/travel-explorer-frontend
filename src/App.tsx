6b4fb8397c410baba24d3f2a952efe928ec5d2c4
Anastasiya Katanova <garbage90@yandex.ru>
2022-09-17 16:56:04 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 function App() {
+  const value = '123';
+
   return (
     <>
       <h1>Welcome, John!</h1>
       <button>OK</button>
+      <Page value={value} />
     </>
   );
 }
 
+const Page = ({value}: {value: boolean}) {
+  return (
+    <div>{value}</div>
+  )
+}
 export default App;
