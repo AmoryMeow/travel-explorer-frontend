523e3ccac1678c8ae4aad88bfb63994d0a358dc0
Anastasiya Katanova <garbage90@yandex.ru>
2022-09-18 20:10:45 +0300

    feat css: add emotion/react, update config, css in props

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,13 @@ function App() {
 
   return (
     <>
-      <h1>{t(`hello:welcome`, { name: "<username>" })}</h1>
+      <h1
+        css={{
+          color: i18n.language === "ru" ? "green" : "blue",
+        }}
+      >
+        {t(`hello:welcome`, { name: "<username>" })}
+      </h1>
       <button>{t("button_ok")}</button>
       <button>{t("button_cancel")}</button>
       <button onClick={changeLanguage("en")}>English</button>
