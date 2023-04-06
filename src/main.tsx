279f4f1573f60471cbb99a74c10867a91238fcf2
Anastasiya Katanova <garbage90@yandex.ru>
2023-04-06 10:35:34 +0200

    feat app: update test, fix imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 import "./index.css";
+import "./i18n/config";
 import { ThemeProvider } from "@package/ui";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
